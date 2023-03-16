b61bf3d1662abfa4370c11896bf9557f0f31f5d5
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-16 11:24:38 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+// const divide = (num1,num2)=> (num1/num2)
+// divide(2000,100)
+
+// const square = a => a*a;
+// const add = (x,y)=>x+y;
+
+function divide(){
+    return 2000/100;
+}
+const square = a => a*a;
+const add = (x,y)=>x+y;
